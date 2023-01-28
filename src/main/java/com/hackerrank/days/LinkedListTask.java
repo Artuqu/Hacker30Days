915f86d9751379c02da0a84e6781be6eb8543318
Artuqu <percikartur@gmail.com>
2023-01-28 10:52:14 +0100

    Linked List

---
+++ src/main/java/com/hackerrank/days/LinkedListTask.java
@@ -0,0 +1,22 @@
+package com.hackerrank.days;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListTask {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Integer> head = new LinkedList<>();
+        int arrayLength = scanner.nextInt();
+
+        for (int i = 0; i < arrayLength; i++) {
+            head.add(scanner.nextInt());
+        }
+
+        for (Integer i : head) {
+            System.out.print(i + " ");
+        }
+    }
+}
