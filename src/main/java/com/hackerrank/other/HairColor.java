60661241ab4299776669422407568bc52484e4b6
Artuqu <percikartur@gmail.com>
2023-02-03 20:28:49 +0100

    Enum, Alias in Person class.

---
+++ src/main/java/com/hackerrank/other/HairColor.java
@@ -0,0 +1,6 @@
+package com.hackerrank.other;
+
+public enum HairColor {
+
+    BLONDE, BLACK, PINK, GREEN, PURPLE
+}
--- src/main/java/com/hackerrank/other/Person.java
+++ src/main/java/com/hackerrank/other/Person.java
@@ -1,10 +1,15 @@
 package com.hackerrank.other;
 
+import static com.hackerrank.other.HairColor.BLACK;
+import static com.hackerrank.other.HairColor.BLONDE;
+
 public class Person {
 
-    int initialAge = 0;
+    private int initialAge = 0;
 
-    int age;
+    private HairColor hairColor = BLONDE;
+
+    private int age;
 
     public Person() {
     }
@@ -16,7 +21,7 @@ public class Person {
                 System.out.println("You are a teenager.");
             } else if (age < 13) {
                 System.out.println("You are young.");
-            }else {
+            } else {
                 System.out.println("You are old.");
             }
         } else {
@@ -28,5 +33,29 @@ public class Person {
 
     public int getAge() {
         return age;
+    }
+
+    public Person(int initialAge, HairColor hairColor, int age) {
+        this.initialAge = initialAge;
+        this.hairColor = hairColor;
+        this.age = age;
+    }
+
+    public HairColor getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(HairColor hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public static void main(String[] args) {
+        Person peterParker = new Person();
+        Person spiderMan = peterParker;
+
+        spiderMan.hairColor = BLACK;
+
+        System.out.println("Hair color of Peter Parker is: " + peterParker.hairColor);
+        System.out.println("Hair color of Spider-man is: " + spiderMan.hairColor);
     }
 }
