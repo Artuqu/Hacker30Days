be704ca9b8ff4be3f5d325642bcd030fa10bd991
Artuqu <percikartur@gmail.com>
2023-02-05 12:31:48 +0100

    Generics second way of solving using generics construct

--- src/main/java/com/hackerrank/days/Generics.java
+++ src/main/java/com/hackerrank/days/Generics.java
@@ -2,35 +2,53 @@ package com.hackerrank.days;
 
 import java.util.*;
 
-public class Generics {
-
+public class Generics<T> {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int integerListSize = scanner.nextInt();
-        List<Integer> integersList = new ArrayList<>(integerListSize);
-        while (integerListSize > 0) {
-            int nextInt = scanner.nextInt();
-            integersList.add(nextInt);
-            integerListSize--;
-        }
-        int stringsListSize = scanner.nextInt();
-        List<String> stringsList = new ArrayList<>(stringsListSize);
-        while (stringsListSize > 0) {
-            String nextString = scanner.next().trim();
-            stringsList.add(nextString);
-            stringsListSize--;
-        }
-        scanner.close();
+        Generics g = new Generics();
+        g.printArray(scanner);
+//        int integerListSize = scanner.nextInt();
+//        List<Integer> integersList = new ArrayList<>(integerListSize);
+//        while (integerListSize > 0) {
+//            int nextInt = scanner.nextInt();
+//            integersList.add(nextInt);
+//            integerListSize--;
+//        }
+//        int stringsListSize = scanner.nextInt();
+//        List<String> stringsList = new ArrayList<>(stringsListSize);
+//        while (stringsListSize > 0) {
+//            String nextString = scanner.next().trim();
+//            stringsList.add(nextString);
+//            stringsListSize--;
+//        }
+//        scanner.close();
+//
+//        for (Integer i : integersList) {
+//            System.out.println(i);
+//        }
+//        for (String s : stringsList) {
+//            System.out.println(s);
+//        }
+    }
 
-        for (Integer i : integersList) {
-            System.out.println(i);
+    public void printArray(Scanner scanner) {
+        List<T> listToPrint = new ArrayList<>();
+        int firstListLength = scanner.nextInt();
+        while (firstListLength > 0) {
+            listToPrint.add((T) scanner.next());
+            firstListLength--;
         }
-        for (String s : stringsList) {
-            System.out.println(s);
-
+        int secondListLength = scanner.nextInt();
+        while (secondListLength > 0) {
+            listToPrint.add((T) scanner.next());
+            secondListLength--;
+        }
+        for (T t : listToPrint) {
+            System.out.println(t);
         }
     }
+
 }
 //7
 //8
