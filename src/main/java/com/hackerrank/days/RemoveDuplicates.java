76ae95ffa0303e2744213605c3b919d77c068973
Artuqu <percikartur@gmail.com>
2023-02-08 14:23:28 +0100

    Printing sorted array without duplicates

---
+++ src/main/java/com/hackerrank/days/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package com.hackerrank.days;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
+        HashSet<Integer> noDuplicates = new HashSet<>();
+        while (arrayLength > 0) {
+            noDuplicates.add(scanner.nextInt());
+            arrayLength--;
+        }
+        LinkedList<Integer> toSort = new LinkedList<>(noDuplicates);
+        Collections.sort(toSort);
+
+        for (Integer i:toSort) {
+            System.out.println(i + " ");
+
+        }
+    }
+}
