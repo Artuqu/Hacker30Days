eae0d2e8380a2f88daf4705a40dd8f805c1749e3
Artuqu <percikartur@gmail.com>
2023-07-09 13:06:19 +0200

    Annotations

---
+++ src/main/java/com/hackerrank/annotations/Single.java
@@ -0,0 +1,24 @@
+package com.hackerrank.annotations;
+
+
+import java.lang.reflect.Method;
+
+public class Single {
+
+    @MySingle(100)
+    public static void myMeth() {
+        Single ob = new Single();
+        try {
+            Method m = ob.getClass().getMethod("myMeth");
+            MySingle anno = m.getAnnotation(MySingle.class);
+            System.out.println(anno.value());
+        } catch (NoSuchMethodException e) {
+            System.out.println("No method found");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMeth();
+    }
+
+}
